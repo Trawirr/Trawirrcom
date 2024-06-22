0522bf120690eab31770d1f4de25f7ad11ba8f0a
Trawirr <gtrawirr@gmail.com>
2024-06-22 18:41:19 +0200

    static files configured using BASE_DIR

--- src/Project/settings.py
+++ src/Project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,10 +120,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
+# Place to store static files in production
+STATIC_ROOT = BASE_DIR / 'local-cdn' / 'static'
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage" 
 
