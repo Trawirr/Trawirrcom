878fc1c2a6ac6498da41212004b613f8018d3fc8
Trawirr <gtrawirr@gmail.com>
2024-06-23 16:54:45 +0200

    htmx installed and configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      black: '#000000',
+      sky: colors.sky,
+      gray: colors.gray
+    },
     extend: {},
   },
   plugins: [],
