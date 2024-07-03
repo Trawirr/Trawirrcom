505c88f1b3283174e1950ef447daee7f26faccd0
Trawirr <gtrawirr@gmail.com>
2024-07-03 23:53:24 +0200

    Card model added

--- src/MainApp/models.py
+++ src/MainApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    name = models.CharField(max_length=30)
+    jpg = models.ImageField(upload_to='images/')
+    gif = models.ImageField(upload_to='images/')
