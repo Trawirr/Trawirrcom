d5134131a6914037ce0815c38c55bc707593d8c1
Trawirr <gtrawirr@gmail.com>
2024-07-04 01:16:15 +0200

    htmx added to template; cards in separate view/template

--- src/MainApp/urls.py
+++ src/MainApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('cards', views.cards_view, name='cards'),
 ]
--- src/MainApp/views.py
+++ src/MainApp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Card
 
 def home_view(request):
     return render(request, "pages/home.html", {})
+
+def cards_view(request):
+    context = {
+        'cards': Card.objects.all(),
+    }
+    print(f"{context=}")
+    return render(request, "pages/cards.html", context)
