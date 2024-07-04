4600b35f52a15a084d3cc117f3fb75e8921e3f3d
Trawirr <gtrawirr@gmail.com>
2024-07-05 00:25:41 +0200

    new color palette used

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
     colors: {
       black: '#000000',
       sky: colors.sky,
-      gray: colors.gray
+      gray: colors.gray,
+      red: colors.red,
+      purple: colors.purple,
+
+      'main_color': {
+        100: '#2D3250',
+        200: '#424769',
+        300: '#7077A1', 
+      },
+      'secondary_color': '#F6B17A',
+
+      'trawirr-gray': {
+        100: '#222831',
+        200: '#393E46'
+      },
+      'trawirr-yellow': '#FFD369',
+      'trawirr-white': '#EEEEEE'
     },
     extend: {},
   },
