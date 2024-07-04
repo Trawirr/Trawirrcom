5da93dd449d7c5cfc6d90fefeffcbaaae6cc71c7
Trawirr <gtrawirr@gmail.com>
2024-07-04 18:17:41 +0200

    Card model updated; Card admin view updated

--- src/MainApp/models.py
+++ src/MainApp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Card(models.Model):
     name = models.CharField(max_length=30)
-    jpg = models.ImageField(upload_to='images/')
-    gif = models.ImageField(upload_to='images/')
+    jpg = models.ImageField(upload_to='images/', blank=True, null=True)
+    gif = models.ImageField(upload_to='images/', blank=True, null=True)
