957a7bea69e8edbef83d47543646f53fffda8525
Trawirr <gtrawirr@gmail.com>
2024-07-09 01:00:05 +0200

    new color palette added and used

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,14 @@ module.exports = {
       },
       'trawirr-yellow': '#FFD369',
       'trawirr-dark-yellow': '#FFC436',
-      'trawirr-white': '#EEEEEE'
+      'trawirr-white': '#EEEEEE',
+
+      'trawirr-green':{
+        100: '#040D12',
+        200: '#183D3D',
+        300: '#5C8374',
+        400: '#93B1A6'
+      }
     },
     extend: {},
   },
