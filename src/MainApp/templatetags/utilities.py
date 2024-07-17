43fb7625cace21dfac19050ebb2cbf34b341d003
Trawirr <gtrawirr@gmail.com>
2024-07-18 01:22:33 +0200

    links to apps added

--- src/MainApp/templatetags/utilities.py
+++ src/MainApp/templatetags/utilities.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name='times') 
 def times(number):
     return range(number)
+
+@register.filter(name='card_url')
+def card_url(card_name):
+    return f"{card_name}-home"
