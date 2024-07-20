c59e242f1b92bcdbaafc4e93ebc4b1b592acf0d4
Trawirr <gtrawirr@gmail.com>
2024-07-20 20:39:58 +0200

    map view first version

--- src/CivApp/urls.py
+++ src/CivApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.civ_home_view, name='civilization-home'),
+    path('map', views.civ_map_view, name='civilization-map'),
 ]
--- src/CivApp/views.py
+++ src/CivApp/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def civ_home_view(request):
     return render(request, "pages/civ/civ_home.html", {})
+
+def civ_map_view(request):
+    x = request.GET.get('x', 0)
+    y = request.GET.get('y', 0)
+    zoom = request.GET.get('zoom', 0)
+    content = {
+        'x': x,
+        'y': y,
+        'zoom': zoom,
+    }
+    return render(request, "pages/civ/civ_map.html", content)
