fea6e6162d0bf7ad07d9717ca349bda1e3033b5a
Trawirr <gtrawirr@gmail.com>
2024-07-22 01:38:24 +0200

    main app migrations added

--- src/MainApp/urls.py
+++ src/MainApp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('cards', views.cards_view, name='cards'),
     path('about-me', views.about_me_view, name='about-me'),
+    path('1', views.home_view, name='processing-home'),
+    path('2', views.home_view, name='gesture-glove-home'),
+    path('3', views.home_view, name='borsukulam-home'),
 ]
