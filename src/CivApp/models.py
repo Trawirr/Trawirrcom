5d12742095267f12ac518c2f3db742d495c17bab
Trawirr <gtrawirr@gmail.com>
2024-08-20 01:55:17 +0200

    first models for generative maps added

--- src/CivApp/models.py
+++ src/CivApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Map(models.Model):
+    name = models.CharField(max_length=20, default="default_map_name")
+
+class Area(models.Model):
+    TYPE_CHOICES = (
+        ("W", "water"),
+        ("L", "land")
+    )
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    area_type = models.CharField(max_length=1, choices=TYPE_CHOICES, default="l")
+
