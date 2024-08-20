6280e26487002d74c6438ac614b2eb3ae487294c
Trawirr <gtrawirr@gmail.com>
2024-08-20 02:08:33 +0200

    map model extended

--- src/CivApp/models.py
+++ src/CivApp/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 class Map(models.Model):
     name = models.CharField(max_length=20, default="default_map_name")
+    seed = models.IntegerField(default=1998)
+    size = models.IntegerField(default=100)
+    octaves = models.CharField(max_length=20, default="3 6 12 24")
+    sea_level = models.FloatField(default=0.5)
+
+    # TO BE ADDED - map categories like 'archipelago', 'pangea' 'islands' 'two continents' etc.
 
 class Area(models.Model):
     TYPE_CHOICES = (
@@ -11,5 +17,5 @@ class Area(models.Model):
     map = models.ForeignKey(Map, on_delete=models.CASCADE)
     x = models.IntegerField()
     y = models.IntegerField()
-    area_type = models.CharField(max_length=1, choices=TYPE_CHOICES, default="l")
+    category = models.CharField(max_length=1, choices=TYPE_CHOICES, default="l")
 
